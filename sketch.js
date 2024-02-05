6cd761bfe7ba48ee8b40927c46155da875071398
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2024-02-04 23:29:54 -0500

    improved visual effects and optimization of memory
    changed so that the particle tail is max 20, also introduced color coding to velocity adn angle

--- sketch.js
+++ sketch.js
@@ -1,28 +1,30 @@
+
+
 let Field;
-let vectorScaling = 50;
+let vectorScaling = .5;
 let gapBetweenRows;
 let gapBetweenCols;
 let counter = 0;
-let sliderx , slidery;
-let resetchance = 0.05;
+
+let resetchance = 0.005;
 
 
 function setup() {
   createCanvas((7 * windowWidth) / 8, (7 * windowHeight) / 8);
-  Field = generateField(10, 10);
+  Field = generateField(40, 40);
+  
 
 
 
 }
 
 function draw() {
-  background(0, 0, 30);
-  counter++;
+  background(40);
 
   translate(width / 2, height / 2);
   scale(1, -1);
-  noStroke();
 
+  noStroke()
   circle(0, 0, 3);
 
   
@@ -32,52 +34,67 @@ function draw() {
   for (j = 0; j < Field[i].length; j++) {
     let positionVector = Field[i][j][Field[i][j].length - 1]; // get the last position vector
     let newposition = positionVector.copy();
-    let velocityVector = Field
-    newposition = newposition.add(VectorFunction(positionVector)); // add velocity to position
+    let velocityVector = VectorFunction(newposition);
+    newposition = newposition.add(velocityVector); // add velocity to position
     Field[i][j].push(newposition); // push the new position
 
+
+
     for(k = 0; k < Field[i][j].length; k++){
-      let alpha = 255 - 10*(Field[i][j].length - k);
-      stroke(200 , 200, 200, alpha);
-      circle(Field[i][j][k].x , Field[i][j][k].y , 2); // show position
+      let alpha = 255 / (k + 1); // Add 1 to avoid division by zero
+      p5.
+      circle(Field[i][j][k].x , Field[i][j][k].y , 5); // show position
     }
+    
+
+    if(Field[i][j].length > 20) {
+      Field[i][j].splice(0, 1)
+    }
+
+    if(velocityVector.mag() < 1) { // if the particle is moving too slow, take away from the front
+      Field[i][j].splice(Field[i][j].length - 1, 1)
+    }
+
 
     if(random(0, 1) < resetchance) {
       Field[i][j] = getInitialFieldPosition(i , j);
     }
+
+
   }
 }
 
-
-}
-
+} // ---------------------------------------------------- end of draw()
 
 
 
 
 
-function visualizeVelocityField(Field) {
-  for (let column of Field) {
-    for (let positionVector of column) {
 
-      velocityVector = VectorFunction(positionVector).mult(vectorScaling)
-      drawArrow(positionVector, velocityVector, 'blue' )
-      
-    }
-  }
-}
+
 
 
 
 
 
 function VectorFunction(positionVector , velocityVector) {
-  let x = vectorScaling*sin(positionVector.y % (2* PI))
-  let y = vectorScaling*cos(positionVector.y % (2 * PI))
-  //let x = vectorScaling*positionVector.x;
-  //let y = vectorScaling*positionVector.y;
-  return createVector(x , y)
-}
+  let x = vectorScaling*log(abs(positionVector.x))
+  let y = vectorScaling*log(abs(positionVector.y));
+  let vector = createVector(x , y);
+  
+  // Calculate the color based on the vector's direction
+  let hue = (vector.heading() + PI) / (2 * PI) * 255;
+  let saturation = 255;
+  let brightness = vector.mag() / vectorScaling * 255;
+  
+  // Set the color mode to HSB and the stroke color to the calculated color
+  colorMode(HSB);
+  //stroke(hue, saturation, brightness, 255);
+  fill(hue, saturation, brightness, 255); // Set the fill color to the same color as the stroke
+  
+  return vector;
+} //---------------------------------------------- end of vectorFunction()
+
 
 
 
@@ -104,16 +121,38 @@ function generateField(nrows, ncols) {
   }
 
   return field;
-}
+} // --------------------------------------------- end of generateField()
+
+
 
 function getInitialFieldPosition(i, j) {
   let x = (i * gapBetweenRows) - width / 2;
-  let y = -1*((j * gapBetweenCols) - height / 2 );
+  let y = -1*((j * gapBetweenCols) - height / 2);
   return [createVector(x , y)]
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+function visualizeVelocityField(Field) {
+  for (let column of Field) {
+    for (let positionVector of column) {
+
+      velocityVector = VectorFunction(positionVector).mult(vectorScaling)
+      drawArrow(positionVector, velocityVector, 'blue' )
+      
+    }
+  }
+}
 
 
 function drawArrow(base, vec, myColor) {
